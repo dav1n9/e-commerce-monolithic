eb8b8c1f651b62948244dc0e0d94259fafa6686f
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-19 18:20:02 +0900

    init commit

---
+++ src/test/java/com/example/fastbuymicroservices/FastBuyMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastbuymicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastBuyMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
