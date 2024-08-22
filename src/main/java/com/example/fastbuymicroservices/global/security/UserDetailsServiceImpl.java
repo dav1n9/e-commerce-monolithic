bd5852ba11a975e02e99fe67ca96f450f896bea8
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-22 13:18:18 +0900

    feat: 로그인 기능 구현
    - 스프링 시큐리티 jwt 로그인 방식

---
+++ src/main/java/com/example/fastbuymicroservices/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.fastbuymicroservices.global.security;
+
+import com.example.fastbuymicroservices.domain.user.entity.User;
+import com.example.fastbuymicroservices.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+        return new UserDetailsImpl(user);
+    }
+
+}
