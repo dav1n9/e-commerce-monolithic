8d642d3c5746c28dd01607ad6260c1e8d5de9371
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-22 22:33:09 +0900

    chore: id 정보 추가

--- src/main/java/com/example/fastbuymicroservices/domain/wishlist/dto/WishlistItemResponseDto.java
+++ src/main/java/com/example/fastbuymicroservices/domain/wishlist/dto/WishlistItemResponseDto.java
@@ -8,11 +8,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class WishlistItemResponseDto {
 
+    private Long id;
     private String itemName;
     private Integer itemPrice;
     private Integer count;
 
     public WishlistItemResponseDto(WishlistItem wishlistItem) {
+        this.id = wishlistItem.getId();
         this.itemName = wishlistItem.getItem().getName();
         this.itemPrice = wishlistItem.getItem().getPrice();
         this.count = wishlistItem.getCount();
