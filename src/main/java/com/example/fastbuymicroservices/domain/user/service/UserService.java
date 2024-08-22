ef32cb42639780ef77f9073bef7101dd24d0e52a
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-22 17:56:54 +0900

    feat: 회원가입시 기본 위시리스트 생성

--- src/main/java/com/example/fastbuymicroservices/domain/user/service/UserService.java
+++ src/main/java/com/example/fastbuymicroservices/domain/user/service/UserService.java
@@ -3,9 +3,12 @@ package com.example.fastbuymicroservices.domain.user.service;
 import com.example.fastbuymicroservices.domain.user.dto.SignupRequestDto;
 import com.example.fastbuymicroservices.domain.user.entity.User;
 import com.example.fastbuymicroservices.domain.user.repository.UserRepository;
+import com.example.fastbuymicroservices.domain.wishlist.entity.Wishlist;
+import com.example.fastbuymicroservices.domain.wishlist.repository.WishlistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,11 +17,12 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final WishlistRepository wishlistRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void signup(SignupRequestDto request) {
 
-        // email 중복확인
         Optional<User> checkEmail = userRepository.findByEmail(request.getEmail());
         if (checkEmail.isPresent()) {
             throw new IllegalArgumentException("중복된 Email 입니다.");
@@ -26,5 +30,11 @@ public class UserService {
 
         User user = request.toEntity(passwordEncoder);
         userRepository.save(user);
+
+        Wishlist wishlist = Wishlist.builder()
+                .name("기본 위시리스트")
+                .user(user)
+                .build();
+        wishlistRepository.save(wishlist);
     }
 }
